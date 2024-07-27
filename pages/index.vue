e7cc7d9ce7dd8c56b1d0d9e76e74a7f56b3dab23
Franck Vienot <68828345+antikkorps@users.noreply.github.com>
2024-07-27 21:54:06 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <section class="self-center flex flex-col flex-1 items-center justify-center">
-      <h1 class="title text-center">Bienvenue sur mon blog</h1>
+      <h1 class="title text-center">Bienvenue sur le blog nd</h1>
       <h2 class="subtitle text-center">ici on parle tech 😁 mais pas que !</h2>
     </section>
 
